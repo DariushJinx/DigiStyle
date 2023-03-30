ad183292321bac06b00b3a530a0bacecbe7401a2
JinxCyrus <www.dariushkhazaei629@gmail.com>
2023-03-30 18:47:36 +0330

    header  section added

---
+++ digiStyle/src/Components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import TopBar from '../TopBar/TopBar';
+import NavBar from '../NavBar/NavBar';
+import SwiperHeader from '../SwiperHeader/SwiperHeader';
+
+export default function Header() {
+  return (
+    <>
+      <TopBar />
+      <NavBar />
+      <SwiperHeader />
+    </>
+  );
+}
