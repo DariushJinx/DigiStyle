a1979dd281c181785e7cfdd0f80dd551236e20f8
JinxCyrus <www.dariushkhazaei629@gmail.com>
2023-03-30 18:53:37 +0330

    iranian designers section and styles added

---
+++ digiStyle/src/Pages/IranianDesignersHome/IranianDesignersHome.js
@@ -0,0 +1,228 @@
+import React from "react";
+import "./IranianDesignersHome.css";
+
+import TopBar from "../../Components/TopBar/TopBar";
+import NavBar from "../../Components/NavBar/NavBar";
+import FooterSection from "../../Components/FooterSection/FooterSection";
+
+export default function IranianDesignersHome() {
+  return (
+    <div className="iranian">
+      <div className="container">
+        <div className="row">
+          <TopBar />
+          <NavBar />
+
+          <h2 className="iranian__title">طراحان لباس ایرانی</h2>
+
+          <div className="iranian__menu col-lg-12 mt-5">
+            <div className="row">
+              <div className="iranian__menu_2_part col-lg-12">
+                <div className="row">
+                  <div className="iranian__menu_2_part_1 col-lg-6">
+                    <img
+                      src="images/JPEG/iranianDesigners/1.jpg"
+                      alt=""
+                      className="iranian__menu_2_part_1_img w-100"
+                    />
+                  </div>
+                  <div className="iranian__menu_2_part_2 col-lg-6">
+                    <img
+                      src="images/JPEG/iranianDesigners/2.jpg"
+                      alt=""
+                      className="iranian__menu_2_part_2_img w-100"
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <div className="iranian__menu_3_part col-lg-12">
+                <div className="row">
+                  <div className="iranian__menu_3_part_1 col-lg-4">
+                    <img
+                      src="images/JPEG/iranianDesigners/3.jpg"
+                      alt=""
+                      className="iranian__menu_3_part_1_img w-100"
+                    />
+                  </div>
+                  <div className="iranian__menu_3_part_2 col-lg-4">
+                    <img
+                      src="images/JPEG/iranianDesigners/4.jpg"
+                      alt=""
+                      className="iranian__menu_3_part_2_img w-100"
+                    />
+                  </div>
+                  <div className="iranian__menu_3_part_3 col-lg-4">
+                    <img
+                      src="images/JPEG/iranianDesigners/5.jpg"
+                      alt=""
+                      className="iranian__menu_3_part_3_img w-100"
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <div className="iranian__menu_3_part_large col-lg-12">
+                <div className="row">
+                  <div className="iranian__menu_3_part_large_1 col-lg-8">
+                    <img
+                      src="images/JPEG/iranianDesigners/6.jpg"
+                      alt=""
+                      className="iranian__menu_3_part_large_1_img w-100"
+                    />
+                  </div>
+                  <div className="iranian__menu_3_part_large_2 col-lg-4">
+                    <div className="row flex-column">
+                      <div className="iranian__menu_2_part_3 col-lg-6 w-100 h-100">
+                        <img
+                          src="images/JPEG/iranianDesigners/7.jpg"
+                          alt=""
+                          className="iranian__menu_3_part_large_2_img w-100"
+                        />
+                      </div>
+                      <div className="iranian__menu_3_part_large_3 col-lg-6 w-100 h-100">
+                        <img
+                          src="images/JPEG/iranianDesigners/8.jpg"
+                          alt=""
+                          className="iranian__menu_3_part_large_3_img w-100"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              {/* second */}
+
+              <div className="iranian__menu_2_part col-lg-12">
+                <div className="row">
+                  <div className="iranian__menu_2_part_1 col-lg-6">
+                    <img
+                      src="images/JPEG/iranianDesigners/9.jpg"
+                      alt=""
+                      className="iranian__menu_2_part_1_img w-100"
+                    />
+                  </div>
+                  <div className="iranian__menu_2_part_2 col-lg-6">
+                    <img
+                      src="images/JPEG/iranianDesigners/10.jpg"
+                      alt=""
+                      className="iranian__menu_2_part_2_img w-100"
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <div className="iranian__menu_3_part col-lg-12">
+                <div className="row">
+                  <div className="iranian__menu_3_part_1 col-lg-4">
+                    <img
+                      src="images/JPEG/iranianDesigners/11.jpg"
+                      alt=""
+                      className="iranian__menu_3_part_1_img w-100"
+                    />
+                  </div>
+                  <div className="iranian__menu_3_part_2 col-lg-4">
+                    <img
+                      src="images/JPEG/iranianDesigners/12.jpg"
+                      alt=""
+                      className="iranian__menu_3_part_2_img w-100"
+                    />
+                  </div>
+                  <div className="iranian__menu_3_part_3 col-lg-4">
+                    <img
+                      src="images/JPEG/iranianDesigners/13.jpg"
+                      alt=""
+                      className="iranian__menu_3_part_3_img w-100"
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <div className="iranian__menu_3_part_large col-lg-12">
+                <div className="row">
+                  <div className="iranian__menu_3_part_large_1 col-lg-8">
+                    <img
+                      src="images/JPEG/iranianDesigners/14.jpg"
+                      alt=""
+                      className="iranian__menu_3_part_large_1_img w-100"
+                    />
+                  </div>
+                  <div className="iranian__menu_3_part_large_2 col-lg-4">
+                    <div className="row flex-column">
+                      <div className="iranian__menu_2_part_3 col-lg-6 w-100 h-100">
+                        <img
+                          src="images/JPEG/iranianDesigners/15.jpg"
+                          alt=""
+                          className="iranian__menu_3_part_large_2_img w-100"
+                        />
+                      </div>
+                      <div className="iranian__menu_3_part_large_3 col-lg-6 w-100 h-100">
+                        <img
+                          src="images/JPEG/iranianDesigners/16.jpg"
+                          alt=""
+                          className="iranian__menu_3_part_large_3_img w-100"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="iranian__menu_2_part col-lg-12">
+                <div className="row">
+                  <div className="iranian__menu_2_part_1 col-lg-6">
+                    <img
+                      src="images/JPEG/iranianDesigners/17.jpg"
+                      alt=""
+                      className="iranian__menu_2_part_1_img w-100"
+                    />
+                  </div>
+                  <div className="iranian__menu_2_part_2 col-lg-6">
+                    <img
+                      src="images/JPEG/iranianDesigners/18.jpg"
+                      alt=""
+                      className="iranian__menu_2_part_2_img w-100"
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <div className="iranian__menu_3_part_large col-lg-12">
+                <div className="row">
+                  <div className="iranian__menu_3_part_large_1 col-lg-8">
+                    <img
+                      src="images/JPEG/iranianDesigners/19.jpg"
+                      alt=""
+                      className="iranian__menu_3_part_large_1_img w-100"
+                    />
+                  </div>
+                  <div className="iranian__menu_3_part_large_2 col-lg-4">
+                    <div className="row flex-column">
+                      <div className="iranian__menu_2_part_3 col-lg-6 w-100 h-100">
+                        <img
+                          src="images/JPEG/iranianDesigners/20.jpg"
+                          alt=""
+                          className="iranian__menu_3_part_large_2_img w-100"
+                        />
+                      </div>
+                      <div className="iranian__menu_3_part_large_3 col-lg-6 w-100 h-100">
+                        <img
+                          src="images/JPEG/iranianDesigners/21.jpg"
+                          alt=""
+                          className="iranian__menu_3_part_large_3_img w-100"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <FooterSection />
+    </div>
+  );
+}
